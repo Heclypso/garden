233a4231b3ea33fd49ded56fa7d0eae290bc34bd
Heclypso <heclypso@gmail.com>
2025-05-28 17:59:16 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Arena from './components/Arena'
+import './Global.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Arena/>
+    </div>
+  )
+}
+
+export default App
